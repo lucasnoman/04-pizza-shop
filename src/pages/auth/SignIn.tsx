4b2a028beec6834b5f5a47a01642fcb5667a12a3
Lucas C. Noman <lucas.noman@outlook.com>
2024-08-22 07:51:19 -0300

    style: creates sign up page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -37,6 +38,9 @@ export function SignIn() {
     <div>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -50,7 +54,7 @@ export function SignIn() {
 
         <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
           <div className="space-y-2">
-            <Label htmlFor="email">Seu e-mail</Label>
+            <Label htmlFor="email">Seu e-mail:</Label>
             <Input id="email" type="email" {...register('email')} />
           </div>
 
